3c28b419db5181951efc99386646c74caf6aa46a
Glauber Gonçalves <sistemas.glauber@gmail.com>
2018-09-09 08:22:24 -0300

    add rotas delete e put para alunos

--- src/routes/alunos.js
+++ src/routes/alunos.js
@@ -2,42 +2,63 @@ module.exports = function(application){
 
 	/****************************
 	 * 
-	 *   Gets
+	 *   Get
 	 * 
 	 ***************************/
 	
 	application.get('/api/v1/alunos/', function(req, res){		
-		application.src.controllers.alunosCtrl.getTodosAlunos(application, req, res)
+		application.src.controllers.alunosCtrl.alunosGet(application, req, res)
 	});
 
 	application.get('/api/v1/alunos/:id', function(req, res){		
-		application.src.controllers.alunosCtrl.getAlunoPorId(application, req, res)
+		application.src.controllers.alunosCtrl.alunosIdGet(application, req, res)
 	});
 
 	application.get('/api/v1/alunos/email/:email', function(req, res){		
-		application.src.controllers.alunosCtrl.getAlunoPorEmail(application, req, res)
+		application.src.controllers.alunosCtrl.alunoEmailGet(application, req, res)
 	});
 
-	application.get('/api/v1/alunos/segue/:id', function(req, res){		
-		application.src.controllers.alunosCtrl.getDisciplinasSeguidas(application, req, res)
+	application.get('/api/v1/alunos/turmas/seguidas/:id', function(req, res){		
+		application.src.controllers.alunosCtrl.alunosTurmasSeguidasIdGet(application, req, res)
 	});
 
 	/****************************
 	 * 
-	 *   Posts
+	 *   Post
 	 * 
 	 ***************************/
 
-	application.post('/api/v1/alunos/seguir/', function(req, res){		
-		application.src.controllers.alunosCtrl.seguirDisciplina(application, req, res)
+	application.post('/api/v1/alunos/', function(req, res){		
+		application.src.controllers.alunosCtrl.alunosPost(application, req, res)
 	});
 
-	application.post('/api/v1/alunos/', function(req, res){		
-		application.src.controllers.alunosCtrl.insertAluno(application, req, res)
+	application.post('/api/v1/alunos/seguirturma/', function(req, res){		
+		application.src.controllers.alunosCtrl.alunoSeguirTurmaPost(application, req, res)
 	});
 
 	application.post('/api/v1/alunos/login/', function(req, res){		
 		application.src.controllers.alunosCtrl.login(application, req, res)
 	});
+
+	/****************************
+	 * 
+	 *   Delete
+	 * 
+	 ***************************/
+	
+	application.delete('/api/v1/alunos/', function(req, res){			
+		application.src.controllers.alunosCtrl.alunosIdDelete(application, req, res)
+	});
+
+	/****************************
+	 * 
+	 *   Put
+	 * 
+	 ***************************/
+	
+	application.put('/api/v1/alunos/', function(req, res){			
+		application.src.controllers.alunosCtrl.alunosPut(application, req, res)
+	});
+
 
 }
